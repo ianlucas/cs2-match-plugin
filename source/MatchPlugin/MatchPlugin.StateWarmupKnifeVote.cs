e572b01aebc203607e14dc7ac8e6cc620b79fbec
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-09-24 21:09:18 -0300

    refactor: use native to switch teams

--- source/MatchPlugin/MatchPlugin.StateWarmupKnifeVote.cs
+++ source/MatchPlugin/MatchPlugin.StateWarmupKnifeVote.cs
@@ -151,12 +151,8 @@ public class StateWarmupKnifeVote(Match match) : StateWarmup(match)
         if (decision == KnifeRoundVote.Switch)
         {
             foreach (var team in Match.Teams)
-            {
                 team.StartingTeam = UtilitiesX.ToggleCsTeam(team.StartingTeam);
-                ServerX.SetTeamName(team.StartingTeam, team.ServerName);
-                foreach (var player in team.Players)
-                    player.Controller?.ChangeTeam(team.StartingTeam);
-            }
+            UtilitiesX.GetGameRules().HandleSwapTeams();
         }
         _didLiveStart = true;
         Match.SetState(new StateLive(Match));
