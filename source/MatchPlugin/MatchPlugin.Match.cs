7d49bd3cdecad8135dbd9b270945315fb3b06813
Ian Lucas <ianlucas_ferreira@hotmail.com>
2024-10-01 09:39:34 -0300

    chore: add logging for sending events

--- source/MatchPlugin/MatchPlugin.Match.cs
+++ source/MatchPlugin/MatchPlugin.Match.cs
@@ -3,7 +3,9 @@
 *  Licensed under the MIT License. See License.txt in the project root for license information.
 *--------------------------------------------------------------------------------------------*/
 
+using System;
 using System.Diagnostics;
+using System.Reflection;
 using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core.Translations;
 using CounterStrikeSharp.API.Modules.Cvars;
@@ -81,6 +83,8 @@ public class Match
 
     public void SendEvent(object @event)
     {
+        PropertyInfo? propertyInfo = @event.GetType().GetProperty("event");
+        Log($"type={propertyInfo?.GetValue(@event)}");
         if (EventsUrl != null)
             ServerX.SendJson(EventsUrl, @event);
     }
